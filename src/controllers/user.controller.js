9885bd424f045cb278b4d9263c4bd137d2ddef09
Asif Hussain <asifkhatian2@gmail.com>
2025-09-29 16:46:24 +0500

    Add getWatchHistory endpoint to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import jwt from 'jsonwebtoken'
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId)=>{
     try {
@@ -333,4 +334,48 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
   })
 
 
-export { registerUser,loginUser ,logout,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile};
+  const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+      {$match:{
+        _id:new mongoose.Types.ObjectId(req.user._id)
+      }},
+      {
+        $lookup:{
+          from:"videos",
+          localField:"watchHistory",
+          foreignField:"_id",
+          as:"watchHistory",
+          pipeline:[
+            {
+              $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner",
+                pipeline:[
+                  {
+                    $project:{
+                      fullName:1,
+                      username:1,
+                      avatar:1
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              $addFields:{
+                owner:{
+                  $first: "$owner"
+                }
+              }
+            }
+          ]
+        }
+      }
+    ])
+
+    res.status(200).json(new ApiResponse(200,user[0].getWatchHistory,"Watch History fetched successfully"))
+  })
+
+export { registerUser,loginUser ,logout,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory};
